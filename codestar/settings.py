5e69a4412999441945be0bcfe6b3d41d7190151d
Alex Curnow <alex@curnows.com>
2024-09-09 10:10:26 +0000

    Test commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^2i%89o(usq7@k!wd3^gmx!3%$ecxzidk7kks#6i387xm#yy$^'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alexcurnow9-djangoblogp-1b95hbwtx33.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -87,6 +87,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/"
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
