6049d344e5ef6bba951f79a5f042f02c3bad4ecb
Alex Curnow <alex@curnows.com>
2024-09-09 11:39:43 +0000

    test commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -94,7 +94,8 @@ DATABASES = {
 
 CSRF_TRUSTED_ORIGINS = [
     "https://8000-ajgreaves-blog-lesson-pl-lfv6kgfdws.us2.codeanyapp.com",
-    "https://*.herokuapp.com"
+    "https://*.herokuapp.com",
+    "https://*.gitpod.io"
 ]
 
 # Password validation
