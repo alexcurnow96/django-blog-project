b9c21508b4a8ba2e661c2c2749f5913d68dec69b
Alex Curnow <alex@curnows.com>
2024-09-12 15:00:02 +0000

    Test commit 2

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-alexcurnow9-djangoblogp-1b95hbwtx33.ws.codeinstitute-ide.net",
